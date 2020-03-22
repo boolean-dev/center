f30c901fea516f32e9d3ad78d700e8e64fe8407e
boolean-dev <boolean.yan@gmail.com>
2020-03-22 21:41:20 +0800

    feat: add thymeleaf and static resource

--- center-web/src/main/java/com/booleandev/center/web/controller/MainController.java
+++ center-web/src/main/java/com/booleandev/center/web/controller/MainController.java
@@ -15,15 +15,15 @@ import java.util.Map;
  * @title: MainController
  * @date 2020/3/13 16:50
  */
-@RestController
+//@RestController
 public class MainController {
 
-    @GetMapping("/**")
+    /*@GetMapping("/**")
     public Object index() {
         Map<String,Object> result = new HashMap<>(3);
         result.put("code", 200);
         result.put("message", "请求成功");
         result.put("data", Arrays.asList("1", "2"));
         return result;
-    }
+    }*/
 }
